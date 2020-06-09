827be96be34f111218d1c9ece2393676252e18a9
Nitin <nitinramadoss@gmail.com>
2020-06-09 23:33:59 +0000

    Enables deleteservlet to delete all the entities with just once call to the datastore delete function

--- portfolio/src/main/java/com/google/sps/servlets/DeleteDataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteDataServlet.java
@@ -17,11 +17,14 @@ import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.sps.data.Comment;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -36,10 +39,12 @@ public class DeleteDataServlet extends HttpServlet
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     Query query = new Query("Comment");
     PreparedQuery results = datastore.prepare(query);
+    List<Key> removeComments = new ArrayList<Key>();
 
     for(Entity e: results.asIterable()){
-        datastore.delete(e.getKey());
+       removeComments.add(e.getKey());
     }
+    datastore.delete(removeComments);
 
     response.getWriter().println();
   }
