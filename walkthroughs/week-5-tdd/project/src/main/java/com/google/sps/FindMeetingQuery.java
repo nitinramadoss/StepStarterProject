e8984eb75901dff7114e63240b56d3fdf0540444
Nitin <nitinramadoss@gmail.com>
2020-06-22 16:40:24 +0000

    Adds support for optional attendees in scheduling algorithm

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -13,46 +13,82 @@
 // limitations under the License.
 package com.google.sps;
 
+import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    List<TimeRange> availableTimes = new ArrayList<TimeRange>();
-    List<TimeRange> unavailableTimes = new ArrayList<TimeRange>();
-
+    List<TimeRange> availableTimesForMandatory = new ArrayList<TimeRange>();
+    List<TimeRange> availableTimesForOptional = new ArrayList<TimeRange>();
+    List<TimeRange> unavailableTimesForMandatory = new ArrayList<TimeRange>();
+    List<TimeRange> unavailableTimesForOptional = new ArrayList<TimeRange>();
+    
     for (Event e : events) {
-        if (!attendeesFree(e, request)) {
-            unavailableTimes.add(e.getWhen());
+        if (!mandatoryAttendeesFree(e, request)) {
+            unavailableTimesForMandatory.add(e.getWhen());
+        }
+
+        if (!optionalAttendeesFree(e, request)) {
+            unavailableTimesForOptional.add(e.getWhen());
         }
     }
 
-    Collections.sort(unavailableTimes, TimeRange.ORDER_BY_START);
+    Collections.sort(unavailableTimesForMandatory, TimeRange.ORDER_BY_START);
+    Collections.sort(unavailableTimesForOptional, TimeRange.ORDER_BY_START);
 
-    if (unavailableTimes.size() > 0) {
-        combineOverlaps(unavailableTimes, unavailableTimes.get(0), 0);
+    if (unavailableTimesForMandatory.size() > 0) { // Do this only for mandatory attendees since only one optional attendee exists
+        combineOverlaps(unavailableTimesForMandatory, unavailableTimesForMandatory.get(0), 0);
     }
 
-    int start = 0;
-    for (TimeRange range : unavailableTimes) {
-        TimeRange availableRange = TimeRange.fromStartEnd(start, range.start(), false);
+    findAvailableRanges(unavailableTimesForMandatory, availableTimesForMandatory, request);
+    findAvailableRanges(unavailableTimesForOptional, availableTimesForOptional, request);
 
-        addAvailableRanges(availableTimes, availableRange, request);
-        
-        start = range.end();
-    }
+    List<TimeRange> finalAvailableTimes = new ArrayList<TimeRange>();
 
-    TimeRange lastAvailableRange = TimeRange.fromStartEnd(start, TimeRange.END_OF_DAY+1, false);
-
-    addAvailableRanges(availableTimes, lastAvailableRange, request);
+    if (request.getOptionalAttendees().size() == 0) {
+        finalAvailableTimes = availableTimesForMandatory;
+    } else if (request.getAttendees().size() == 0) {
+        finalAvailableTimes = availableTimesForOptional; 
+    } else if (availableTimesForMandatory.size() == 0) {
+        finalAvailableTimes = availableTimesForOptional;
+    } else if (availableTimesForOptional.size() == 0) { 
+        finalAvailableTimes = availableTimesForMandatory;
+    } else {
+        for (TimeRange m : availableTimesForMandatory) {
+            for (TimeRange o : availableTimesForOptional) {
+                TimeRange encompassed = null;
     
-    return availableTimes;   
+                if (m.start() >= o.start() && m.end() <= o.end()) {
+                    encompassed = m;
+                } else if (o.start() >= m.start() && o.end() <= m.end()) {
+                    encompassed = o;
+                }
+
+                if (encompassed != null) {
+                    finalAvailableTimes.add(encompassed);
+                }               
+            }   
+        }
+
+        if (finalAvailableTimes.size() == 0) {
+            finalAvailableTimes = availableTimesForMandatory;
+        }
+    }
+
+    return finalAvailableTimes;   
   }
  
-  public boolean attendeesFree(Event e, MeetingRequest request){
+  public boolean mandatoryAttendeesFree(Event e, MeetingRequest request){
     return Collections.disjoint(e.getAttendees(), request.getAttendees());
+  }
+
+  public boolean optionalAttendeesFree(Event e, MeetingRequest request){
+    return Collections.disjoint(e.getAttendees(), request.getOptionalAttendees());
   }
 
   public void addAvailableRanges(List<TimeRange> availableTimes, TimeRange range, MeetingRequest request){
@@ -82,5 +118,20 @@ public final class FindMeetingQuery {
     }
 
     combineOverlaps(unavailableTimes, current, index + 1);
+  }
+
+  public void findAvailableRanges(List<TimeRange> unavailableTimes, List<TimeRange> availableTimes, MeetingRequest request){
+        int start = 0;
+        for (TimeRange range : unavailableTimes) {
+            TimeRange availableRange = TimeRange.fromStartEnd(start, range.start(), false);
+            
+            addAvailableRanges(availableTimes, availableRange, request);
+            
+            start = range.end();
+        }
+
+    TimeRange lastAvailableRange = TimeRange.fromStartEnd(start, TimeRange.END_OF_DAY + 1, false);
+
+    addAvailableRanges(availableTimes, lastAvailableRange, request);
   }
 }
